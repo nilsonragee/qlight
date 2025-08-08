cf8620b986abf2d10f2179182de3fadafa688daf
nilsonragee <nilsonragee@gmail.com>
2025-08-08 03:05:24 +0300

    common: New `For` macros for simplicity and readability

--- src/common.h
+++ src/common.h
@@ -4,7 +4,128 @@
 #include "types.h"
 #include "allocator.h"
 
-// #define For( count )  for ( uint32_t it = 0; it < count; count += 1 )
+// `( count & 0 )` is a trick to make compiler do type deduction from `count` variable.
+// If you would assign to 0, the compiler would infer from the literal - 0 is `int`.
+/*
+	ForNamed( i, 3 ) {
+		log_info( "i=%u", i );
+	}
+
+	is equivalent to:
+
+	for ( auto i = ( 3 & 0 ); i < 3; i += 1 ) {
+		log_info( "i=%u", i );
+	}
+*/
+#define ForNamed( variable, count )  for ( auto variable = ( count & 0 ); variable < count; variable += 1 )
+
+// Use `s64` so the index can not underflow if `count` is unsigned.
+// It could be countered by checking for `count`'s type max value, but how do you get the type?
+// Too much hustle, keep it simple.
+/*
+	ForNamedBackwards( i, 3 ) {
+		log_info( "i=%u", i );
+	}
+
+	is equivalent to:
+
+	for ( s64 i = 3 - 1; i >= 0; i -= 1 ) {
+		log_info( "i=%u", i );
+	}
+*/
+#define ForNamedBackwards( variable, count )  for ( s64 variable = count - 1; variable >= 0; variable -= 1 )
+
+/*
+	For( 3 ) {
+		log_info( "it_index=%u", it_index );
+	}
+
+	is equivalent to:
+
+	for ( auto it_index = ( 3 & 0 ); it_index < 3; it_index += 1 ) {
+		log_info( "it_index=%u", it_index );
+	}
+*/
+#define For( count )  ForNamed( it_index, count )
+
+/*
+	ForBackwards( 3 ) {
+		log_info( "it_index=%u", it_index );
+	}
+
+	is equivalent to:
+
+	for ( auto it_index = 3 - 1; it_index >= 0; it_index -= 1 ) {
+		log_info( "it_index=%u", it_index );
+	}
+*/
+#define ForBackwards( count )  ForNamedBackwards( it_index, count )
+
+#define For2( count )  ForNamed( it2_index, count )
+#define For2Backwards( count )  ForNamedBackwards( it2_index, count )
+
+#define For3( count )  ForNamed( it3_index, count )
+#define For3Backwards( count )  ForNamedBackwards( it3_index, count )
+
+/*
+	StringView_ASCII str = "Hello World";
+	ForIt( str.data, str.size ) {
+		log_info( "str[%2u]: '%c'", it_index, *it );
+	}}
+
+	is equivalent to:
+
+	StringView_ASCII str = "Hello World";
+	for ( auto it_index = str.size; it_index < str.size; it_index += 1 ) {
+		auto *it = &pointer[ it_index ];
+		{
+			log_info( "str[%2u]: '%c'", it_index, *it );
+		}
+	}
+*/
+#define ForIt( pointer, count )  \
+		For( count ) {  \
+			auto *it = &pointer[ it_index ];
+
+/*
+	StringView_ASCII str = "Hello World";
+	ForItBackwards( str.data, str.size ) {
+		log_info( "str[%2u]: '%c'", it_index, *it );
+	}}
+
+	is equivalent to:
+
+	StringView_ASCII str = "Hello World";
+	for ( s64 it_index = str.size - 1; it_index >= 0; it_index -= 1 ) {
+		auto *it = &pointer[ it_index ];
+		{
+			log_info( "str[%2u]: '%c'", it_index, *it );
+		}
+	}
+*/
+#define ForItBackwards( pointer, count )  \
+		ForBackwards( count ) {  \
+			auto *it = &pointer[ it_index ];
+
+
+#define ForIt2( pointer, count )  \
+		For2( count ) {  \
+			auto *it2 = &pointer[ it2_index ];
+
+#define ForIt2Backwards( pointer, count )  \
+		For2Backwards( count ) {  \
+			auto *it2 = &pointer[ it2_index ];
+
+
+#define ForIt3( pointer, count )  \
+		For3( count ) {  \
+			auto *it3 = &pointer[ it3_index ];
+
+#define ForIt3Backwards( pointer, count )  \
+		For3Backwards( count ) {  \
+			auto *it3 = &pointer[ it3_index ];
+
+
 #define ARRAY_SIZE( array )  sizeof( array ) / sizeof( array[ 0 ] )
 
 s8 QL_clamp(s8 x, s8 min, s8 max);
